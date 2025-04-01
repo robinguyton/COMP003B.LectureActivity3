9b797535b7088383b012b55cf66f8791bda7db57
RobinGuyton <gladiatorrobin@aol.com>
2025-03-31 19:18:08 -0700

    modified product controller

--- COMP003B.LectureActivity3/Controllers/ProductController.cs
+++ COMP003B.LectureActivity3/Controllers/ProductController.cs
@@ -1,12 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using COMP003B.LectureActivity3.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace COMP003B.LectureActivity3.Controllers
 {
+    // The [Route("product")] attribute specifies that the controller will hangle requests that atart eith "/product".
+    [Route("product")]
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        //Get: product/create
+        [HttpGet("create")]
+        public IActionResult Create()
         {
+            //The View method returns a view result that renders a view to the response.
             return View();
         }
+
+        //Post: product/create
+        [HttpPost("create")]
+        public IActionResult Create([FromForm] Product product)
+        {
+            //The ModelState.IsValid property checks if the model state is valid.
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+
+            //The RedirectToAction() method returns a redirect result that redirects to an action.
+            return RedirectToAction("Success", product);
+        }
+
+        // Get: product/success
+        [Route("success")]
+
+        public IActionResult Success(Product product)
+        {
+            //The View ()method returns a view that renders a view to the response.
+            return View(product);
+
+
+        }
     }
+
 }
