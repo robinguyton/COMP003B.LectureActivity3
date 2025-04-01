83a773d66a8ce46bb6d7ab4705cd9a3988e8d9d2
RobinGuyton <gladiatorrobin@aol.com>
2025-03-31 19:45:47 -0700

    modded views-create

--- COMP003B.LectureActivity3/Controllers/ShopController.cs
+++ COMP003B.LectureActivity3/Controllers/ShopController.cs
@@ -4,9 +4,23 @@ namespace COMP003B.LectureActivity3.Controllers
 {
     public class ShopController : Controller
     {
+        // Get: /Shop/
+        [HttpGet]
         public IActionResult Index()
         {
+            // The View() method returns a view result that renders a view to the response.
             return View();
         }
+
+        // Get: /Shop/Details/5
+        [HttpGet]
+
+        public IActionResult Details(int id)
+        {
+            // The View () method returns a view result that renders a view to the response.
+            // The id parameter is passed to the view.
+            return View(id);
+        }
+
     }
 }
