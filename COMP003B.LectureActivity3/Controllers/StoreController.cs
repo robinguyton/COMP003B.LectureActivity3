bda41154f26c84b39e0e543c3cfc124f37465067
RobinGuyton <gladiatorrobin@aol.com>
2025-03-31 19:27:00 -0700

    modified  Store Controller

--- COMP003B.LectureActivity3/Controllers/StoreController.cs
+++ COMP003B.LectureActivity3/Controllers/StoreController.cs
@@ -2,11 +2,27 @@
 
 namespace COMP003B.LectureActivity3.Controllers
 {
+    // The StoreController class is a controller that handles requests for ther store section of the website.
+    // The [Route("store")] attribute specifies that the controller will handle requests that start with "/store".
+    [Route("store")]
     public class StoreController : Controller
     {
-        public IActionResult Index()
+        // Get store/product/5
+        [Route("product/{id:int}")]
+        public IActionResult ViewProduct(int id)
+    {
+        // The View() method returns a view result that renders a view to the response.
+        // The id parameter is passed to the view.
+            return View(id);
+    }
+    
+    
+    // Get: store/product/search?category=electronics
+    [HttpGet("product/search")]
+        public IActionResult Search([FromQuery] string category)
+
         {
-            return View();
+            return View("Search", category);
         }
     }
 }
